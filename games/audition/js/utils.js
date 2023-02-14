60d6564abde9322659d71124d06a4c1efa1c2379
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-02-15 01:28:53 +0700

    audition - beat up dance

---
+++ games/audition/js/utils.js
@@ -0,0 +1,15 @@
+function show(id) {
+    document.getElementById(id).style.display = 'block'
+}
+
+function hide(id) {
+    document.getElementById(id).style.display = 'none'
+}
+
+function setKey(key, id) {
+    document.getElementById(id).src = "images/" + key + ".png"
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
