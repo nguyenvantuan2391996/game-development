d8a4236003f5cd10d992b505c773ba731385ccec
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-02-27 02:23:39 +0700

    computer vs computer

--- games/caro/js/constants.js
+++ games/caro/js/constants.js
@@ -5,7 +5,8 @@ const O = "o"
 const WIN = "win"
 const DRAW = "draw"
 const TWO_PLAYER = "2-players"
-const COMPUTER = "play-computer"
+const COMPUTER = "player-computer"
+const COMPUTER_COMPUTER = "computer-computer"
 const MAP_SCORE_COMPUTER = new Map([
     [5, Infinity], [4, 2000], [3, 500], [2, 300], [1, 100]
 ])
