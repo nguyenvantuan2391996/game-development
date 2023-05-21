0ddef133df2aacf0ffc657a56a20a1d4d37007a0
tuannguyen <tuan.nguyen25@tiki.vn>
2023-05-21 17:32:56 +0700

    feat: adding music player

---
+++ games/music-player/js/constants.js
@@ -0,0 +1,7 @@
+const API_KEY = 'AIzaSyBuv4KNR2wKlm5dMp-FRZ9hLEKGbqkMErc';
+
+const NOT_FOUND_SONG = "The song is not found"
+
+const VIDEO_YOUTUBE_ID = "video_youtube_id"
+
+const NOT_FOUND = "not found"
---
+++ games/music-player/js/youtubeAPI.js
@@ -0,0 +1,42 @@
+async function searchKey(keyword) {
+  return await searchYouTube(keyword, API_KEY);
+}
+
+async function searchYouTube(keyword, apiKey) {
+  // Perform keyword search using YouTube Data API v3
+  const searchResponse = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${encodeURIComponent(
+      keyword
+    )}&key=${apiKey}`
+  );
+  const searchResult = await searchResponse.json();
+
+  return searchResult.items[0].id.videoId;
+}
+
+async function getInfoSong(id) {
+  let res = {
+    title: NOT_FOUND,
+    artist: NOT_FOUND,
+    thumbnail: NOT_FOUND,
+  };
+
+  await fetch(
+    `https://www.googleapis.com/youtube/v3/videos?id=` +
+      id +
+      `&part=snippet&key=` +
+      API_KEY
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const video = data.items[0];
+      res.artist = video.snippet.channelTitle;
+      res.title = video.snippet.title;
+      res.thumbnail = video.snippet.thumbnails.standard.url;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  return res;
+}
